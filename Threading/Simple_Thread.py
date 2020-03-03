f0cf86209ef0197c93d956db31e22dc228913312
kane601 <790970430@qq.com>
2020-03-04 00:21:58 +0800

    Threading Modeule

---
+++ Threading/Simple_Thread.py
@@ -0,0 +1,31 @@
+#threading模块
+#普通创建方式
+
+import threading
+import time
+
+def run(n):
+    print("task", n)
+    time.sleep(1)
+    print('2s')
+    time.sleep(1)
+    print('1s')
+    time.sleep(1)
+    print('0s')
+    time.sleep(1)
+
+def run2(n):
+    print("task", n)
+    time.sleep(1)
+    print('2s')
+    time.sleep(1)
+    print('1s')
+    time.sleep(1)
+    print('0s')
+    time.sleep(1)
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=run, args=("t1",))
+    t2 = threading.Thread(target=run2, args=("t2",))
+    t1.start()
+    t2.start()
